c54201b7dfa63e7847287dd23f73edd330f59618
tanvirdeveloper01 <144343942+tanvirdeveloper01@users.noreply.github.com>
2024-01-06 12:23:07 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+var tl =gsap.timeline()
+tl.from("#nav h3 ",{
+    y:-50,
+    opacity:0,
+    delay:0.4,
+    duration:1,
+    stagger:0.5
+}) 
+tl.from(".list-item ",{
+    y:-50,
+    opacity:0,
+    delay:0.5,
+    duration:1,
+    stagger:0.5
+   
+}) 
+tl.from("#main h1",{
+    x:-500,
+    opacity:0,
+    delay:1,
+    duration:0.8,
+    stagger:0.4
+})
+tl.from("#main .newbutton",{
+    x:-500,
+    opacity:0,
+    
+    duration:0.8,
+    stagger:0.4,
+    
+    
+})
